a1c2f89afbe269f513f5b211db9810242a4f5f35
Keter <lkbhndu22@gmail.com>
2025-06-25 14:36:12 +0700

    path fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
 // @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-// @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
 // @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/ktrct.github.io", // Change this to match your exact repository name
-  assetPrefix: "/ktrct.github.io", // Add this line
+  // No basePath needed for username.github.io repos
+  // No assetPrefix needed for username.github.io repos
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-
