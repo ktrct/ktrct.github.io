e16fcffc0ca29926366195cc00ca092dcf323cae
Keter <lkbhndu22@gmail.com>
2025-06-26 15:23:23 +0700

    move nav bar to z-50

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Nav() {
     return (
-        <nav className="sticky top-0 flex bg-blue-100 pt-8 pb-8 w-full text-black transition duration-100">
+        <nav className="sticky top-0 flex bg-blue-100 pt-8 pb-8 w-full text-black transition duration-100 z-50">
             <Link className="pl-10 hover:text-amber-800" href="/">
             Home
             </Link>
