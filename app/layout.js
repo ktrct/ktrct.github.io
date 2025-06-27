f7ac1907c112068bc0a3f75e77bee90bece781ce
Keter <lkbhndu22@gmail.com>
2025-06-27 15:21:03 +0700

    try to fix favicon

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"></link>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
