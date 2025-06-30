2ad750add708e29b50f2e0deb76d31c3c84e22f6
Keter <lkbhndu22@gmail.com>
2025-06-30 14:21:30 +0700

    move repo link and removed side scrollbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import Image from "next/image";
 import { ScrollLinked } from './ScrollLinked';
 import { useRef, useState, useEffect } from 'react';
 
@@ -24,19 +25,38 @@ export function Nav() {
     }, []);
 
     return (
-        <nav
-            ref={navRef}
-            className="sticky top-0 flex items-center space-x-10 bg-blue-100 pt-6 pb-6 w-full text-black z-30 *:z-49"
-        >
-            {navHeight !== null && <ScrollLinked navHeight={navHeight} />}
+    <nav
+        ref={navRef}
+        className="sticky top-0 flex items-center space-x-10 bg-blue-100 pt-6 pb-6 w-full text-black z-30 *:z-49"
+    >
+        {navHeight !== null && <ScrollLinked navHeight={navHeight} />}
 
-            <Link className="hover:text-cyan-600 pl-6 z-50" href="/">
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"/>
-                <span className="material-symbols-outlined" style={{ fontSize: '50px' }}>
-                    home
-                </span>
-            </Link>
-            <Link href="/alt">alt</Link>
-        </nav>
+        <Link className="hover:text-cyan-600 transition-colors pl-6 z-50" href="/">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
+            <span className="material-symbols-outlined" style={{ fontSize: '50px' }}>
+                home
+            </span>
+        </Link>
+
+        <Link href="/alt">alt</Link>
+
+        <div className="ml-auto pr-6">
+            <a
+                className="rounded-full transition-colors flex items-center justify-center bg-black gap-2 hover:bg-cyan-600 
+                font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto text-white"
+                href="https://github.com/ktrct/ktrct.github.io"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Image
+                    src="/github-mark-white.svg"
+                    alt="Github logomark"
+                    width={20}
+                    height={20}
+                />
+                Repo
+            </a>
+        </div>
+    </nav>
     );
 }
